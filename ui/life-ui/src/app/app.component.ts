2d7dbb3dab57cf68b4bf3e35f7b4f75c84ab9dbd
Mohsen Amirian <mohsen.amirian@yahoo.com>
2020-11-20 21:07:12 +0330

    cors policy added

--- ui/life-ui/src/app/app.component.ts
+++ ui/life-ui/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'life-ui';
+
+  constructor(private http: HttpClient) {
+    http.get('https://localhost:44306/api/word').subscribe((x) => {
+        console.log(x);
+    });
+  }
 }
--- web-api-core/LifeService/LifeService/Startup.cs
+++ web-api-core/LifeService/LifeService/Startup.cs
@@ -18,6 +18,8 @@ namespace LifeService
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,6 +30,15 @@ namespace LifeService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:4200");
+                                  });
+            });
+
             services.AddDbContext<LifeContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:LifeDB"]));
             services.AddScoped<IDataRepository<Word>, WordRepository>();
             services.AddControllers();
@@ -44,6 +55,8 @@ namespace LifeService
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
